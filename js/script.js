d2c8e2d66a38e85d0b5467ce2880c9ff6d09637b
Andrea <andreanuzzo@hotmail.it>
2021-11-17 16:07:41 +0100

    draft

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const squareGame = (whereInsert, numSquare, nameclass) =>{
+    for (let i = 0; i < num; i++){
+        whereInsert.innerHTML += `<div id="${nameclass}">${i}</div>`
+    }
+}
+
+
+const outerSquare 
+
+const easy = document.querySelector(".LevelEasy");
+const medium = document.querySelector(".LevelMedium");
+const hard = document.querySelector(".LevelHard");
+
+easy.addEventListener("click", function(){
+    squareGame(outerSquare, 100, squareEasy)
+});
