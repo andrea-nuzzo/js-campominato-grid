2955bbe417a3eada0eab4473c8b9e7d62d4caac0
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2021-11-18 15:34:10 +0100

    completed exercise

--- js/script.js
+++ js/script.js
@@ -1,16 +1,62 @@
-const squareGame = (whereInsert, numSquare, nameclass) =>{
-    for (let i = 0; i < num; i++){
-        whereInsert.innerHTML += `<div id="${nameclass}">${i}</div>`
+// Selezioniamo Play
+document.getElementById("play").addEventListener("click", function(){
+    Minesweeper()
+})
+
+// La funzione crea la grglia con il gioco
+function Minesweeper(){
+    
+    //Svuoto il container ad ogni inizio altrimenti i box si sommeranno sotto
+    document.querySelector('.outerSquare').innerHTML = '';
+    
+    // Seleziona la toolbar e faccio scegliere il livello
+    const selectLevel = parseInt(document.getElementById("level").value);
+
+    // BoxNumb rappresenta il totale dei box che sranno presenti
+    let boxNumb;
+
+    switch(selectLevel){
+        //Easy
+        case 1:
+            boxNumb = 100;
+            break;
+        //Medium
+        case 2:
+            boxNumb = 81;
+            break;
+        //Hard
+        case 3:
+            boxNumb = 49;
+            break;
     }
+    // boxRow rappresenta il numero di box per riga
+    boxRow = Math.sqrt(boxNumb);
+
+
+    // Questa funzione crea i vari box
+    function boxCreator(numbInsideBox) {
+        const box = document.createElement("div");
+        box.classList.add("square");
+        const width =  `calc(100% / ${boxRow})`;
+        box.style.width = width;
+        box.innerHTML = numbInsideBox;
+        return box;
+    }
+
+
+    // Questo ciclo inserisce i box all'interno del container a seconda la seleziona che abbiamo fatto in precedenza
+    const containerBox = document.querySelector('.outerSquare');
+
+
+    for(let i = 1; i <= boxNumb; i++){
+        box = boxCreator(i);
+        containerBox.append(box);
+        
+        box.addEventListener('click', function() {
+            this.classList.add('selected');
+        });
+    }
+
+   
+
 }
-
-
-const outerSquare 
-
-const easy = document.querySelector(".LevelEasy");
-const medium = document.querySelector(".LevelMedium");
-const hard = document.querySelector(".LevelHard");
-
-easy.addEventListener("click", function(){
-    squareGame(outerSquare, 100, squareEasy)
-});
